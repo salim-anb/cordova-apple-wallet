78a3944617e09fe043cf23c84e17698a418742b2
salim-anb <salim_ajabbar@yahoo.co.in>
2019-07-23 15:18:26 +0300

    update .m file
    update the network name for visamadaco

--- src/ios/CDVAppleWallet.m
+++ src/ios/CDVAppleWallet.m
@@ -231,6 +231,9 @@ typedef void (^completedPaymentProcessHandler)(PKAddPaymentPassRequest *request)
         if([[paymentNetwork uppercaseString] isEqualToString:@"MASTERCARD"]) {
             configuration.paymentNetwork = PKPaymentNetworkMasterCard;
         }
+        if([[paymentNetwork uppercaseString] isEqualToString:@"VISAMADACO"]) {
+            configuration.paymentNetwork = PKPaymentNetworkMada;
+        }
 
         // Present view controller
         self.addPaymentPassModal = [[PKAddPaymentPassViewController alloc] initWithRequestConfiguration:configuration delegate:self];
